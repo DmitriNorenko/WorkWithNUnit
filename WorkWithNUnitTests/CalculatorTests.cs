7cccddf4a08351686e8530cb6f9043a45009f7ec
Name <email@exaple.ru>
2023-09-21 11:15:29 +0300

    Добавил класс CalculatorTests

---
+++ WorkWithNUnitTests/CalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WorkWithNUnit;
+
+namespace WorkWithNUnitTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Additional(10, 4) == 14);
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Subtraction(10, 4) == 6);
+        }
+        [Test]
+
+        public void MiltiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(10, 4) == 40);
+        }
+        [Test]
+
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(10, 2) == 5);
+        }
+    }
+}
